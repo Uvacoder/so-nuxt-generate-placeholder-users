00ce9ea9fbdcc9bd25c39ae5d4d786a1f263bf01
Konstantin BIFERT <konstantin.bifert@gmail.com>
2021-07-20 02:13:06 +0200

    fix: small adjustments

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,12 +1,15 @@
 <template>
-  <div>User name: {{ user.name }}</div>
+  <div>
+    <div v-if="user">User name: {{ user.name }}</div>
+    <div v-else-if="error">{{ error }}</div>
+  </div>
 </template>
 
 <script>
 export default {
   asyncData({ payload, redirect }) {
     if (payload && Object.entries(payload).length) return { user: payload }
-    else redirect({ name: 'no-user' }) // this will also can users going to `/users/`
+    else return { error: 'This user does not exist' } // this will also can users going to `/users/`
   },
 }
 </script>
